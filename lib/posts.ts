2b6039532ce19ce8f1a74534e3d9938ffc8460b7
yumemi-kawasumi <a_kawasumi@yumemi.co.jp>
2022-02-01 19:54:50 +0900

    fix: add remark

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
@@ -50,12 +52,18 @@ export function getSortedPostsData(): PostData[] {
   });
 }
 
-export function getPostData(id: string): PostData {
+export const getPostData = async (id: string): Promise<PostData> => {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
 
   // Use gray-matter to parse the post metadata section
   const matterResult = matter(fileContents);
+
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
 
   // Combine the data with the id
   return {
@@ -63,9 +71,9 @@ export function getPostData(id: string): PostData {
     ...matterResult.data,
     date: matterResult.data.date,
     title: matterResult.data.title,
-    contentHtml: matterResult.content,
+    contentHtml,
   };
-}
+};
 
 export function getAllPostIds() {
   const fileNames = fs.readdirSync(postsDirectory);
