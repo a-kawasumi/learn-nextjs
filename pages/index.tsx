f9282a6f12406a141bd1e54bb4a49c5effb1e9c2
yumemi-kawasumi <a_kawasumi@yumemi.co.jp>
2022-02-01 16:27:43 +0900

    feat: create blog data

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,24 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData, PostData } from "../lib/posts";
+import utilStyles from "../styles/utils.module.css";
 
-const Home: NextPage = () => {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+interface Props {
+  allPostsData: PostData[];
+}
+
+const Home: NextPage<Props> = (props) => {
+  const { allPostsData } = props;
   return (
     <Layout home>
       <div className={styles.container}>
@@ -16,6 +32,22 @@ const Home: NextPage = () => {
         </Head>
 
         <main className={styles.main}>
+          <section
+            className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
+          >
+            <h2 className={utilStyles.headingLg}>Blog</h2>
+            <ul className={utilStyles.list}>
+              {allPostsData.map(({ id, date, title }) => (
+                <li className={utilStyles.listItem} key={id}>
+                  {title}
+                  <br />
+                  {id}
+                  <br />
+                  {date}
+                </li>
+              ))}
+            </ul>
+          </section>
           <h1 className="title">
             Read{" "}
             <Link href="/posts/first-post">
