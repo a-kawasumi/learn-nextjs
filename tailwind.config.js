f2c10e5763edbce416d8a73e3a49d61525f09752
yumemi-kawasumi <a_kawasumi@yumemi.co.jp>
2022-02-03 12:11:11 +0900

    fix: delete not use setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  content: ["./src/components/**/*.{ts,tsx}", "./src/pages/**/*.{ts,tsx}"],
   // build時に未使用のスタイルを除外
-  purge: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
+  content: ["./src/components/**/*.{ts,tsx}", "./src/pages/**/*.{ts,tsx}"],
 };
